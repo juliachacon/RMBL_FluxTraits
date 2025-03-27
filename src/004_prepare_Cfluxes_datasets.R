563312214ae516b2014a7cee80d7c2fafac3e0df
juliachacon <juliachacon@gmail.com>
2025-03-27 23:59:15 +0100

    revisado

--- src/004_prepare_Cfluxes_datasets.R
+++ src/004_prepare_Cfluxes_datasets.R
@@ -1,4 +1,3 @@
-
 ## Preparing C Fluxes Datasets across RMBL gradient
 ## Original by Julia, improved March 24th 2025
 
@@ -13,8 +12,8 @@ set.seed(12345)
 elevation_rmbl <- readRDS(here("data/processed", "elevation_rmbl.rds"))
 
 # Source trait moments and climate scripts
-source(here("scr", "start_here_moments.R"), echo = TRUE)
-source(here("scr", "start_here_climate_3.R"), echo = TRUE)
+source(here("src", "start_here_moments.R"), echo = TRUE)
+source(here("src", "start_here_climate.R"), echo = TRUE)
 
 # Load NEE data
 load(here("data/processed", "nee_sum_curated.Rdata"))
@@ -25,27 +24,25 @@ mean_nee <- nee %>%
   group_by(site, year, plot, time) %>%
   summarise(nee_lm_mean = mean(nee_lm, na.rm = TRUE), .groups = "drop")
 
-# Split NEE, Reco_day, Reco_night datasets
-NEE <- mean_nee %>% filter(time == "day") %>% rename(nee = nee_lm_mean)
-
+# Split into NEE, Reco_day, and Reco_night
+day <- mean_nee %>% filter(time == "day") %>% rename(nee = nee_lm_mean)
 dayresp <- mean_nee %>% filter(time == "dayresp") %>% rename(reco_day = nee_lm_mean)
-
 nightresp <- mean_nee %>% filter(time == "nightresp") %>% rename(reco_night = nee_lm_mean)
 
-# Calculate GPP (Gross Primary Production)
-cfluxes_day <- left_join(dayresp, NEE, by = c("site", "year", "plot")) %>%
+# Calculate GPP
+cfluxes_day <- left_join(dayresp, day, by = c("site", "year", "plot")) %>%
   mutate(gpp = nee - reco_day) %>%
   select(-time.x, -time.y)
 
-cfluxes_night <- left_join(nightresp, NEE, by = c("site", "year", "plot")) %>%
+cfluxes_night <- left_join(nightresp, day, by = c("site", "year", "plot")) %>%
   mutate(gpp = nee - reco_night) %>%
   select(-time.x, -time.y)
 
-# Add elevation (merge elevation by site)
-data_list <- list(NEE, cfluxes_day, cfluxes_night)
+# Add elevation
+data_list <- list(day, cfluxes_day, cfluxes_night)
 data_list <- lapply(data_list, function(df) left_join(df, elevation_rmbl, by = "site"))
 
-NEE <- data_list[[1]]
+day <- data_list[[1]]
 cfluxes_day <- data_list[[2]]
 cfluxes_night <- data_list[[3]]
 
@@ -67,27 +64,26 @@ NEE <- filter_common(NEE)
 cfluxes_night <- filter_common(cfluxes_night)
 cfluxes_day <- filter_common(cfluxes_day)
 
-# Remove positive respiration values in night data
+# Remove positive respiration values
 cfluxes_night <- cfluxes_night %>% filter(reco_night < 0)
 
-# Merge moments and climate data
-cfluxes_day <- left_join(cfluxes_day, moments)
-cfluxes_night <- left_join(cfluxes_night, moments)
+# Merge with trait moments and climate data
+cfluxes_day <- left_join(cfluxes_day, moments, by = c("site", "year", "plot"))
+cfluxes_night <- left_join(cfluxes_night, moments, by = c("site", "year", "plot"))
 
-cfluxes_day <- left_join(cfluxes_day, climate_data4)
-cfluxes_night <- left_join(cfluxes_night, climate_data4)
+cfluxes_day <- left_join(cfluxes_day, climate_data4, by = c("site", "year"))
+cfluxes_night <- left_join(cfluxes_night, climate_data4, by = c("site", "year"))
 
-# Remove rows with NA in key columns
+
+# Remove rows with missing key trait values
 cfluxes_night <- cfluxes_night %>% drop_na(percent_P, delta13C)
 
-# Quick check
-summary(cfluxes_night)
 
-## Save outputs if needed
-# saveRDS(cfluxes_night, here("clean data", "cfluxes_night_clean.rds"))
-# saveRDS(cfluxes_day, here("clean data", "cfluxes_day_clean.rds"))
+# Save outputs
+saveRDS(cfluxes_night, here("data/processed", "cfluxes_night_clean.rds"))
+saveRDS(cfluxes_day, here("data/processed", "cfluxes_day_clean.rds"))
 
-## checking de aqui para abajo:
+
 
 ## Before continuing: I am going to finally use cfluxes_night
 # This database contains nee, reconight and gpp calculated with reconight
@@ -97,9 +93,6 @@ dim(cfluxes_night) # 220   7
 head(cfluxes_night)
 plot(cfluxes_night$reco_night)
 abline(h=0)
-
-cfluxes_night <- cfluxes_night %>% 
-  filter(reco_night < 0)
 
 dim(cfluxes_night) # 180   7
 plot(cfluxes_night$reco_night)
